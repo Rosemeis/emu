2fce6db786da1524e10928d8f63db2973fb7b4cf
Jonas <meisnerucph@gmail.com>
2019-05-15 12:30:33 +0200

    Added feature for singular matrices

--- flashpcangsd.py
+++ flashpcangsd.py
@@ -17,18 +17,21 @@ from scipy.sparse.linalg import svds
 from sklearn.utils.extmath import randomized_svd
 
 ### Main function ###
-def flashPCAngsd(D, f, e, K, M, M_tole, F, p, svd_method, svd_power, t):
+def flashPCAngsd(D, f, e, K, M, M_tole, F, p, W, s, U, svd_method, svd_power, indf_save, output, t):
 	n, m = D.shape # Dimensions
 	E = np.empty((n, m), dtype=np.float32)
 
-	if F is None:
-		shared.updateE_init(D, f, E, t) # Initiate E
+	if W is None:
+		if F is None:
+			shared.updateE_init(D, f, E, t) # Initiate E
+		else:
+			shared.updateE_init_guided(D, F, p, E, t) # Guided initiation of E
+			del p, F
 	else:
-		shared.updateE_init_guided(D, F, p, E, t)
-		del F, p
+		shared.updateE_SVD(D, E, f, W, s, U, t) # Initiate E based on previous estimates
 
 	if M < 1:
-		print("Missingness not taken into account!")
+		print("Warning, no EM-PCA iterations are performed!")
 
 		# Estimating SVD
 		shared.standardizeMatrix(E, f, t)
@@ -68,6 +71,11 @@ def flashPCAngsd(D, f, e, K, M, M_tole, F, p, svd_method, svd_power, t):
 				print("Estimation of individual allele frequencies has converged.")
 				break
 			prevW = np.copy(W)
+		if indf_save:
+			print("Saving singular matrices for future use (.w.npy, .s.npy, .u.npy).")
+			np.save(output + ".w", W)
+			np.save(output + ".s", s)
+			np.save(output + ".u", U)
 		del W, s, U, prevW
 
 		# Estimating SVD
@@ -98,8 +106,8 @@ parser.add_argument("-m_tole", metavar="FLOAT", type=float, default=1e-5,
 	help="Tolerance for update in estimation of individual allele frequencies (1e-5)")
 parser.add_argument("-t", metavar="INT", type=int, default=1,
 	help="Number of threads")
-parser.add_argument("-maf", metavar="FLOAT", type=float, default=0.05,
-	help="Threshold for minor allele frequencies")
+parser.add_argument("-maf", metavar="FLOAT", type=float, default=0.00,
+	help="Threshold for minor allele frequencies (0.00)")
 parser.add_argument("-selection", action="store_true",
 	help="Perform PC-based selection scan (Galinsky et al. 2016)")
 parser.add_argument("-maf_save", action="store_true",
@@ -107,19 +115,25 @@ parser.add_argument("-maf_save", action="store_true",
 parser.add_argument("-bool_save", action="store_true",
 	help="Save boolean vector used in MAF filtering (Binary)")
 parser.add_argument("-indf_save", action="store_true",
-	help="Save estimated individual allele frequencies")
+	help="Save estimated singular matrices")
 parser.add_argument("-index", metavar="FILE",
 	help="Index for guided allele frequencies")
 parser.add_argument("-svd", metavar="STRING", default="arpack",
 	help="Method for performing truncated SVD (ARPACK/Randomized)")
 parser.add_argument("-svd_power", metavar="INT", type=int, default=4,
 	help="Number of power iterations in randomized SVD")
+parser.add_argument("-w", metavar="FILE",
+	help="Left singular matrix (.w.npy)")
+parser.add_argument("-s", metavar="FILE",
+	help="Singular values (.s.npy)")
+parser.add_argument("-u", metavar="FILE",
+	help="Right singular matrix (.u.npy)")
 parser.add_argument("-o", metavar="OUTPUT", help="Prefix output file name", default="flash")
 args = parser.parse_args()
 
 
 ### Caller ###
-print("FlashPCAngsd 0.3\n")
+print("FlashPCAngsd 0.4\n")
 
 # Set K
 if args.k is None:
@@ -132,7 +146,6 @@ print("Reading in single-read sampling matrix from binary NumPy file.")
 # Read from binary NumPy file. Expects np.int8 data format
 D = np.load(args.input)
 assert D.dtype == np.int8, "NumPy array must be of 8-bit integer format (np.int8)!"
-
 n, m = D.shape
 
 # Population allele frequencies
@@ -157,23 +170,32 @@ if args.index is not None:
 	F = np.empty((m, max(p)+1), dtype=np.float32)
 	shared.estimateF_guided(D, f, F, p, args.t)
 else:
-	p = None
-	F = None
+	p, F = None, None
+
+# Use eigenvectors from previous run
+if args.w is not None:
+	assert args.s is not None, "Must supply both -s and -u along with -w!"
+	assert args.u is not None, "Must supply both -s and -u along with -w!"
+	print("Reading singular matrices from previous run.")
+	W = np.load(args.w)
+	assert W.shape[0] == n, "Number of samples in W must match D!"
+	s = np.load(args.s)
+	assert s.shape[0] == args.e, "Number of eigenvectors in W, s and U must match -e!"
+	U = np.load(args.u)
+	assert U.shape[1] == m, "Number of sites in U must match D!"
+else:
+	W, s, U = None, None, None
 
 # FlashPCAngsd
 print("Performing FlashPCAngsd.")
 print("Using " + str(args.e) + " eigenvector(s).")
-V, s, U = flashPCAngsd(D, f, args.e, K, args.m, args.m_tole, F, p, args.svd, args.svd_power, args.t)
+V, s, U = flashPCAngsd(D, f, args.e, K, args.m, args.m_tole, F, p, W, s, U, args.svd, \
+	args.svd_power, args.indf_save, args.o, args.t)
 
 print("Saving eigenvector(s) as " + args.o + ".eigenvecs.npy (Binary).")
 np.save(args.o + ".eigenvecs", V.astype(float, copy=False))
 print("Saving eigenvalue(s) as " + args.o + ".eigenvals (Text).")
 np.savetxt(args.o + ".eigenvals", s**2/m)
-
-if args.indf_save:
-	print("Saving individual allele frequencies as " + args.o + ".indf.npy (Binary).")
-	np.save(args.o + ".indf", np.dot(V[:, :args.e]*s[:args.e], U[:args.e]).astype(float, copy=False))
-del V, s # Clear memory
 
 if args.selection:
 	print("Performing selection scan along each PC.")
